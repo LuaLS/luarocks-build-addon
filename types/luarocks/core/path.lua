e0a28280614f895231a79ffed419e23477a91c85
goldenstein64 <goldnstein64@gmail.com>
2026-03-24 04:05:26 -0400

    doc: add types for more luarocks.path fields

--- types/luarocks/core/path.lua
+++ types/luarocks/core/path.lua
@@ -7,4 +7,8 @@ local path = {}
 ---@return string
 function path.rocks_tree_to_string(tree) end
 
+---@param tree string | luarocks.core.Tree
+---@return string
+function path.deploy_lua_dir(tree) end
+
 return path
--- types/luarocks/path.lua
+++ types/luarocks/path.lua
@@ -1,8 +1,28 @@
-local core_path = require("luarocks.core.path")
+---@meta
+
+local core_path = require("luarocks.core.path") --[[@as luarocks.core.path]]
 
 ---@class luarocks.path
 local path = {}
 
 path.rocks_tree_to_string = core_path.rocks_tree_to_string
+path.deploy_lua_dir = core_path.deploy_lua_dir
+
+---Get the local installation directory (prefix) for a package.
+---@param name string -- the package name
+---@param version string -- the package version
+---@param tree? string | luarocks.core.Tree -- if given, specifies the local tree to use
+---@return string install_dir -- the resulting path -- does not guarantee that the package (and by extension, the path) exists
+function path.install_dir(name, version, tree) end
+
+---Get the local installation directory for Lua modules of a package.
+---@param name string -- the package name
+---@param version string -- the package version
+---@param tree? string | luarocks.core.Tree -- if given, specifies the local tree to use
+---@return string lua_dir -- the resulting path -- does not guarantee that the package (and by extension, the path) exists
+function path.lua_dir(name, version, tree) end
+
+---@param tree string | luarocks.core.Tree
+function path.use_tree(tree) end
 
 return path
